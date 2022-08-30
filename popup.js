d7fe0768690157a82f79217596101e8c698d2617
MaksimTimchenko <105758079+MaksimTimchenko@users.noreply.github.com>
2022-08-30 20:32:55 +0300

    fixed bug

--- popup.js
+++ popup.js
@@ -1,18 +1,8 @@
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  changeColor.style.background = color;
 });
-
-
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
 
 
   function setPageBackgroundColor() {
