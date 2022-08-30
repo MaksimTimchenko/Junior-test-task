1c336ca0a01af02f5e6e58b65184f1772076af03
MaksimTimchenko <105758079+MaksimTimchenko@users.noreply.github.com>
2022-08-30 15:08:44 +0300

    first commit

---
+++ content.js
@@ -0,0 +1,141 @@
+
+const run = () => {
+
+const inputs = document.querySelectorAll('input[type=text]');
+const contenteditableElements = document.querySelectorAll('[contenteditable=true]');
+const dictionary = {
+    'Cat': ['Dog', 'Rat', 'bat'],
+    'Helo': ['hello', 'Help', 'Hell'],
+    'heldp': ['help', 'held', 'hello']
+};
+let keywords = Object.keys(dictionary);
+
+
+const OpenpopupReplacementWord = (word,textArea) => {
+    const popup = document.createElement('div');
+        chrome.storage.sync.get('color', (data) =>{
+        popup.classList.add('pops');
+        popup.style.background = data.color;
+    });
+    dictionary[word].forEach(item => {
+        popup.innerHTML += `
+        <button data-wordBtn class="word_btn">${item}</button>
+        `;
+    });
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            document.querySelector('.pops') ? document.querySelector('.pops').remove() : null;
+            textArea.focus();
+        }
+    });
+    return document.body.append(popup);
+    };
+
+const ContenteditableReplacement  = (word,textArea,index) => {
+    OpenpopupReplacementWord(word,textArea);
+    const btns = document.querySelectorAll('[data-wordBtn]');
+        btns.forEach(btnWord => {
+        btnWord.addEventListener('click', (e) => {
+        let inputValueArr = textArea.innerHTML.split(' ');             
+        let newWord = e.target.textContent;
+        inputValueArr[index] = newWord;
+        textArea.textContent = inputValueArr.join(' ').replace(/\&nbsp;/g, '');
+        document.querySelector('.pops').remove();
+        textArea.focus();
+        });
+        });     
+    };  
+
+const changeWord  = (word,res,input) => {
+    if (keywords.includes(word[word.length -1])) {
+        OpenpopupReplacementWord(word[word.length -1],input);
+        const btns = document.querySelectorAll('[data-wordBtn]');
+        btns.forEach(btnWord => {
+            btnWord.addEventListener('click', (e) => {
+                let newWord = e.target.innerHTML;
+                let newReg = new RegExp(word[word.length-1] + '$');
+                let result = res.trim().replace(newReg, newWord);
+                input.value = result;
+                input.innerHTML= result;
+                res = '';
+                input.setAttribute('value', result);
+                document.querySelector('.pops').remove();
+                input.focus();
+            });
+        });
+    }
+};  
+
+const changeTypingValue = (item) => {
+    let res = '';
+    item.oninput = () => {
+    res = item.textContent;
+    };
+    item.addEventListener('keydown', (e) => {
+        if (e.keyCode == 32) {
+            let word = res.split(' ');
+            changeWord(word, res,item);
+        }
+    });
+};
+
+
+const findPositionOfWord = (item) => {
+        let sel, word = "";
+        if (window.getSelection && (sel = window.getSelection()).modify) {
+            let selectedRange = sel.getRangeAt(0);
+            sel.collapseToStart();
+            sel.modify("move", "backward", "word");
+            sel.modify("extend", "forward", "word");
+            word = sel.toString().trim(); 
+            let char = sel.anchorOffset;
+            let countStr = sel.anchorNode.textContent.substr(0, char);
+            var wordNumber = (countStr === "") ? 0 : countStr.trim().split(/\s/).length ;
+            sel.removeAllRanges();
+            sel.addRange(selectedRange);
+
+        }
+       
+        if (keywords.includes(word)) {
+            ContenteditableReplacement(word,item,wordNumber);
+        } 
+    
+};
+
+const changeWordExistenText = (elem) => {
+    elem.forEach(item => {      
+        item.addEventListener('click', (e)=> {
+            findPositionOfWord(item);           
+        });
+
+        item.addEventListener('keydown', (e) => {
+            if (e.keyCode === 38 || e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 40 ) {
+                findPositionOfWord(item);
+            }
+        });
+        
+        changeTypingValue(item);
+     });
+};
+
+
+inputs.forEach(input => {
+    let res = '';
+    input.oninput  = () => {
+        res = input.value;
+    };
+input.addEventListener('keydown', (e) => {
+    if (e.keyCode == 32) {
+            let word = res.split(' ');
+            changeWord(word, res,input);
+        }
+    
+    });
+    
+});
+changeWordExistenText(contenteditableElements);
+};
+run();
+
+
+
